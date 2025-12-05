d5c9a6b9b02a8928d5a48c69f38bf80ae223e132
p3rtub4d00 <containerbeerpvh@gmail.com>
2025-12-05 12:19:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 require('dotenv').config();
 const app = require('./src/app');
-const connectDB = require('./src/config/db');
 
 const PORT = process.env.PORT || 3000;
 
-// Conectar ao Banco antes de iniciar o servidor
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`🚀 Servidor rodando na porta ${PORT}`);
-    });
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor (Modo Visual) rodando na porta ${PORT}`);
 });
